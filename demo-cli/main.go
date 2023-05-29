d388c521d1cd572672fc4950a20e7b25d087fb40
socylx <socylx@163.com>
2023-05-29 18:33:28 +0800

    demo-cli

---
+++ demo-cli/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"github.com/micro/micro/v3/service"
+	pb "github.com/socylx/laracom/demo-service/proto/demo"
+	"log"
+	"time"
+)
+
+func main() {
+	srv := service.New()
+	srv.Init()
+
+	client := pb.NewDemoService("demo-service", srv.Client())
+	rsp, err := client.SayHello(context.TODO(), &pb.DemoRequest{Name: "学院君"})
+	if err != nil {
+		log.Fatalf("服务调用失败：%v", err)
+		return
+	}
+	log.Println(rsp.Text)
+
+	// let's delay the process for exiting for reasons you'll see below
+	time.Sleep(time.Second * 5)
+}
