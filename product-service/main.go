cf34cbec7244a183aaed4d128e39cc23d1ea9d10
socylx <socylx@163.com>
2023-06-19 17:46:13 +0800

    product-service

---
+++ product-service/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	micro "github.com/micro/micro/v3/service"
+	"github.com/socylx/laracom/product-service/db"
+	"github.com/socylx/laracom/product-service/handler"
+	"github.com/socylx/laracom/product-service/model"
+	pb "github.com/socylx/laracom/product-service/proto/product"
+	"github.com/socylx/laracom/product-service/repo"
+	"log"
+)
+
+func main() {
+	// 创建数据库连接，程序退出时断开连接
+	database, err := db.CreateConnection()
+	defer func() { _ = database.Close() }()
+
+	if err != nil {
+		log.Fatalf("Could not connect to DB: %v", err)
+	}
+
+	// 数据库迁移
+	database.AutoMigrate(&model.Product{})
+
+	// 初始化 Repo 实例用于后续数据库操作
+	productRepo := &repo.ProductRepository{Db: database}
+
+	// 以下是 Micro 创建微服务流程
+	srv := micro.New(
+		micro.Name("product"),
+		micro.Version("latest"), // 新增接口版本参数
+	)
+	srv.Init()
+
+	// 注册处理器
+	_ = pb.RegisterProductServiceHandler(srv.Server(), &handler.ProductService{ProductRepo: productRepo})
+
+	// 启动商品服务
+	if err := srv.Run(); err != nil {
+		fmt.Println(err)
+	}
+}
